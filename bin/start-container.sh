6d435df1d2918c7c2693fb4a89c037812a8d53a1
Tobias Hüske <t.hueske@mittwald.de>
2024-05-01 09:51:56 +0200

    Fix error when there are no container with backup laebl

--- bin/start-container.sh
+++ bin/start-container.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-docker start $(docker ps -qa -f 'label=backups=true')
+# Liste der IDs der Docker-Container abrufen, die das Label 'backups=true' haben
+containers=$(docker ps -qa -f 'label=backups=true')
 
-if [ $? -eq 0 ]; then
-    echo "Docker-Container erfolgreich gestartet."
+if [ -z "$containers" ]; then
+    # Wenn keine Container mit dem Label gefunden wurden
+    echo "Keine Docker-Container mit dem Label 'backups=true' gefunden."
 else
-    echo "Fehler: Docker-Container konnten nicht gestartet werden."
-    /scripts/report-status.sh down
-    exit 1
+    # Wenn Container mit dem Label gefunden wurden, diese starten
+    docker start $containers
+    if [ $? -eq 0 ]; then
+        echo "Docker-Container erfolgreich gestartet."
+    else
+        echo "Fehler: Docker-Container konnten nicht gestartet werden."
+        /scripts/report-status.sh down
+        exit 1
+    fi
 fi
 
 exit 0
--- bin/stop-container.sh
+++ bin/stop-container.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-docker stop $(docker ps -qa -f 'label=backups=true')
+# Liste der IDs der Docker-Container abrufen, die das Label 'backups=true' haben
+containers=$(docker ps -qa -f 'label=backups=true')
 
-if [ $? -eq 0 ]; then
-    echo "Docker-Container erfolgreich gestoppt."
+if [ -z "$containers" ]; then
+    # Wenn keine Container mit dem Label gefunden wurden
+    echo "Keine Docker-Container mit dem Label 'backups=true' gefunden."
 else
-    echo "Fehler: Docker-Container konnten nicht gestoppt werden."
-    /scripts/report-status.sh down
-    exit 1
+    # Wenn Container mit dem Label gefunden wurden, diese stoppen
+    docker stop $containers
+    if [ $? -eq 0 ]; then
+        echo "Docker-Container erfolgreich gestoppt."
+    else
+        echo "Fehler: Docker-Container konnten nicht gestoppt werden."
+        /scripts/report-status.sh down
+        exit 1
+    fi
 fi
 
 exit 0
