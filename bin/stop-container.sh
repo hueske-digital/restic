c87ce79d95539f8f19a26330137810f3d544ff3e
Tobias Hüske <thueske@users.noreply.github.com>
2025-01-03 22:13:55 +0100

    Update stop-container.sh

--- bin/stop-container.sh
+++ bin/stop-container.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Überprüfen, ob die NOTIFY_URL gesetzt ist
+if [ -z "${NOTIFY_URL}" ]; then
+    echo "Fehler: Die Umgebungsvariable NOTIFY_URL ist nicht gesetzt."
+    exit 2
+fi
+
+# Wenn NOTIFY_URL das Wort "ping" enthält, sende einen Start-Request
+if [[ "${NOTIFY_URL}" == *ping* ]]; then
+    FULL_URL="${NOTIFY_URL}/start"
+    response=$(curl --user-agent "resticker" -s -o /dev/null -w "%{http_code}" "${FULL_URL}")
+    if [ "$response" -eq 200 ]; then
+        echo "Start-Notification erfolgreich an ${FULL_URL} gesendet."
+    else
+        echo "Fehler beim Senden der Start-Notification. HTTP-Statuscode: $response"
+        exit 3
+    fi
+fi
+
 # Liste der IDs der Docker-Container abrufen, die das Label 'backups=true' haben
 containers=$(docker ps -qa -f 'label=backups=true')
 
